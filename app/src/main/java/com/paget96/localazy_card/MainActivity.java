fa933a9d5e2b37db0b4dc2619e1c03fe3d8d345a
Paget96 <mPaget96@gmail.com>
2021-03-19 16:58:38 +0100

    Added few more changes to card style

--- app/src/main/java/com/paget96/localazy_card/MainActivity.java
+++ app/src/main/java/com/paget96/localazy_card/MainActivity.java
@@ -3,6 +3,7 @@ package com.paget96.localazy_card;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.paget96.localazycard.LocalazyCard;
 
@@ -14,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LocalazyCard localazyCard = findViewById(R.id.localazyCard);
-
         localazyCard.setIcon(R.drawable.ic_localazy);
         localazyCard.setTitle(getString(R.string.default_title));
         localazyCard.setSummaryText(getString(R.string.default_summary));
         localazyCard.setTranslateButton("https://localazy.com");
+
+        localazyCard.setRadius(24);
+        localazyCard.setStrokeColor(ContextCompat.getColor(this, R.color.design_default_color_primary));
+        localazyCard.setStrokeWidth(2);
     }
 }
